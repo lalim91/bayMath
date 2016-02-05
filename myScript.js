e8188e6597698a08009d9e79f5c10fcb34f3c859
Leslie Ann Lim <Lalim@Leslies-MacBook-Air.local>
2016-02-04 18:02:10 -0800

    added 3 click handlers, functions: store num, store operator, update display

--- myScript.js
+++ myScript.js
@@ -1,40 +1,75 @@
+var input_storage = [''];
+var current_index = 0;
 
 
+$(document).ready(function(){
 
-    //callback function defined
-    function newFunc(type, value, item) {
-        switch (value) {
-            case undefined:
-                $('#display_area').html("");
-                break;
-            default:
-                $('#display_area').html(value);
-                break;
-        }
-    }
-    // my_calculator - creates a new calculator object
-    var my_calculator = new calculator(newFunc);
-    //after DOM load add click handlers to all buttons
-    $(document).ready(function () {
-        $('.btn').on('click', function () {
-            var val = $(this).text();
-            switch (val) {
-                case 'AC':
-                    my_calculator.allClear();
-                    break;
-                default:
-                    my_calculator.addItem($(this).text());
-                    break;
-            }
-        });
+    $('.number').on('click', function(){//get the values of number and store them in store_number function
+        store_number($(this).text());
     });
 
+    $('.operator').on('click', function(){
+        store_operator($(this).text());
+    });
 
+    $('#equal').on('click', function(){
+       do_math();
+    });
 
+});
 
-//@purpose:display value of number in the display area
+//@purpose:store numbers that are clicked in a variable; update display
 //@params:
-    //type: 'itemAdded', 'calculated', 'error'
-    //value: string or number
-    //error
-//returns:
+    //button_value
+//@return
+    //none
+//@global
+    //input_storage: where everything clicked will be stored in strings
+    //current_index: the index of the most current position; where we are right now
+function store_number(button_value){
+    input_storage[current_index] = button_value;
+    update_display();
+}
+
+//@purpose:store operators that are clicked in a variable; update display
+//@params:
+    //button_value
+//@return
+    //none
+//@global
+    //input_storage: where everything clicked will be stored in strings
+    //current_index: the index of the most current position; where we are right now
+function store_operator(button_value){
+    current_index +=1;
+    input_storage[current_index] = button_value;
+    current_index +=1;
+    input_storage[current_index] = "";
+    update_display();
+}
+
+//@purpose:display whatever is in the array input_storage
+//@params:
+    //none
+//@return
+    //none
+//@global
+    //input_storage: where everything clicked will be stored in strings
+function update_display(){
+    var array_to_str = "";
+    for(var i=0; i<input_storage.length; i++){
+        array_to_str += input_storage[i];
+    }
+    $('#display').text(array_to_str);
+}
+
+//@purpose: do math based from parameters; decide which equation to use
+//@params:
+    //op1: first operand
+    //op2: second operand
+    //operator: the operator to perform
+//@return
+    //the result of math
+//@global
+    //input_storage: where everything clicked will be stored in strings
+    //current_index: the index of the most current position; where we are right now
+function
